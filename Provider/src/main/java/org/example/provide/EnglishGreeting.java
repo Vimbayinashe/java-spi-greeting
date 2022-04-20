7485658db206362b90151cdfca0ce03415d6a1a6
Vimbayinashe <vimbayinashemandaza@gmail.com>
2022-04-20 11:31:24 +0200

    Add EnglishGreeting

---
+++ Provider/src/main/java/org/example/provide/EnglishGreeting.java
@@ -0,0 +1,12 @@
+package org.example.provide;
+
+import org.example.greet.Greeting;
+
+public class EnglishGreeting implements Greeting {
+
+    @Override
+    public String greet() {
+        return "Hello!";
+    }
+
+}
