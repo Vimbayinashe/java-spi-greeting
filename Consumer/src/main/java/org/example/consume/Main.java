e614e3bc3213718e7e66a19256e30afc5d6a0eb9
Vimbayinashe <vimbayinashemandaza@gmail.com>
2022-04-20 14:39:05 +0200

    Update ConsumerMain

--- Consumer/src/main/java/org/example/consume/Main.java
+++ Consumer/src/main/java/org/example/consume/Main.java
@@ -11,8 +11,18 @@ public class Main {
         ServiceLoader<Greeting> serviceLoader = ServiceLoader.load(Greeting.class); //classes that implement Greeting interface
 
         for (Greeting greeting : serviceLoader) {
+
+            System.out.println(greeting.getClass().getSimpleName());
+
             System.out.println(greeting.greet());
         }
+
+        System.out.println("-------------------------------");
+
+        serviceLoader.stream()
+                .map(ServiceLoader.Provider::get)
+                .filter(greeting -> greeting.getClass().getSimpleName().startsWith("Swedish"))  // reflections
+                .forEach(greetingProvider -> System.out.println(greetingProvider.greet()));
 
     }
 
