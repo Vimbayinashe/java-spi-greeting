fac92b5d60eb875125ae2e6686e795e3d028dd4c
Vimbayinashe <vimbayinashemandaza@gmail.com>
2022-04-20 14:58:58 +0200

    Add Lazy Loading alterntive

--- Consumer/src/main/java/org/example/consume/Main.java
+++ Consumer/src/main/java/org/example/consume/Main.java
@@ -10,19 +10,24 @@ public class Main {
 
         ServiceLoader<Greeting> serviceLoader = ServiceLoader.load(Greeting.class); //classes that implement Greeting interface
 
-        for (Greeting greeting : serviceLoader) {
-
-            System.out.println(greeting.getClass().getSimpleName());
-
-            System.out.println(greeting.greet());
-        }
-
-        System.out.println("-------------------------------");
-
-        serviceLoader.stream()
+        // Alt 2: Lazy Loading
+        var greeting = serviceLoader.stream()
+                .filter(provider -> provider.type().getSimpleName().startsWith("Swedish"))
                 .map(ServiceLoader.Provider::get)
-                .filter(greeting -> greeting.getClass().getSimpleName().startsWith("Swedish"))  // reflections
-                .forEach(greetingProvider -> System.out.println(greetingProvider.greet()));
+                .findAny();
+
+        greeting.ifPresent(greeting1 -> System.out.println(greeting1.greet()));
+
+        // Alt 2: Eager Loading     (expensive)
+//        var greetings = serviceLoader.stream()
+//                .map(ServiceLoader.Provider::get)
+//                .filter(greeting -> greeting.getClass().getSimpleName().startsWith("Swedish"))  // reflections
+//                .toList();
+
+//        for (Greeting greeting : greetings) {
+//            if (greeting.getClass().getSimpleName().startsWith("Swedish"))
+//                System.out.println(greeting.greet());
+//        }
 
     }
 
