14ac673b554148a3caa24e77a0222ee5c8275cf9
Vimbayinashe <vimbayinashemandaza@gmail.com>
2022-04-22 10:26:31 +0200

    Add greeting in constructor

--- Provider/src/main/java/org/example/provide/EnglishGreeting.java
+++ Provider/src/main/java/org/example/provide/EnglishGreeting.java
@@ -4,6 +4,10 @@ import org.example.greet.Greeting;
 
 public class EnglishGreeting implements Greeting {
 
+    public EnglishGreeting() {
+        System.out.println("English created");
+    }
+
     @Override
     public String greet() {
         return "Hello!";
