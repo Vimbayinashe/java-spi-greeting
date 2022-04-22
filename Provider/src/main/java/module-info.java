76ece6e5fa1d4f6dd0fae4c0e66b8dfaa78c9abb
Vimbayinashe <vimbayinashemandaza@gmail.com>
2022-04-22 10:25:56 +0200

    Add example of JetBrains @NotNull annotation

--- Provider/src/main/java/module-info.java
+++ Provider/src/main/java/module-info.java
@@ -3,6 +3,7 @@ import org.example.provide.EnglishGreeting;
 import org.example.provide.SwedishGreeting;
 
 module org.example.greetingsprovider {
+    requires org.jetbrains.annotations;
     requires org.example.greeting;
     provides Greeting with SwedishGreeting, EnglishGreeting;
 }
--- Provider/src/main/java/org/example/provide/SwedishGreeting.java
+++ Provider/src/main/java/org/example/provide/SwedishGreeting.java
@@ -2,6 +2,7 @@ package org.example.provide;
 
 import org.example.greet.Address;
 import org.example.greet.Greeting;
+import org.jetbrains.annotations.NotNull;
 
 @Address("swedish")
 public class SwedishGreeting implements Greeting {
@@ -15,6 +16,9 @@ public class SwedishGreeting implements Greeting {
         return "Hej!";
     }
 
-
+//    @NotNull                        // method cannot return null -> throws compilation error
+//    public  String testMethod() {
+//        return null;
+//    }
 
 }
